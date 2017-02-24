5af45f9dc923d6b020a00e3a30506de13cc80c61
Hailz <hailey083@yahoo.com>
2017-02-24 11:12:03 -0800

    Hard code added to flatten two deep arrays

--- sortArrays.js
+++ sortArrays.js
@@ -1,4 +1,4 @@
-//Flatten for sorted number arrays:
+//Flatten for one deep sorted number arrays:
 var array1= [1,2,[4,8],5,[6],[7,8],10];
 var flattened =[];
 console.log(array1);
@@ -8,7 +8,7 @@ function flatten(){
 }
 flatten();
 
-// Flatten for non sorted number arrays:
+// Flatten for one deep non sorted number arrays:
 var array2= [11, 3, [20, 1], 49, [8], 7];
 var flattened2 =[];
 function flatten2(){
@@ -21,7 +21,7 @@ function sortNumbers(a,b){
 };
 flatten2();
 
-// Flatten for word arrays:
+// Flatten for one deep word arrays:
 var wordsArray=['appa', ['brady', 'marsha'], 'buddy', 'little'];
 var flattenedWords = [];
 console.log(wordsArray);
@@ -32,3 +32,26 @@ function flattenWords(){
   console.log(flattenedWords);
 };
 flattenWords();
+
+//Hard code to flatten multiple deep arrays:
+var array2Deep= [11, 3, [[20, 1], 49], [8], 7];
+var flattened1stLevel =[];
+var flattened2ndLevel =[];
+console.log(array2Deep);
+function flatten1stLevel(){
+  flattened1stLevel = array2Deep.reduce((acc, cur) => acc.concat(cur), []);
+  console.log(flattened1stLevel);
+}
+function sortNumbers(a,b){
+  return(a-b);
+};
+flatten1stLevel();
+function flatten2ndLevel(){
+  flattened2ndLevel = flattened1stLevel.reduce((acc, cur) => acc.concat(cur), []);
+  flattened2ndLevel.sort(sortNumbers);
+  console.log(flattened2ndLevel);
+}
+function sortNumbers(a,b){
+  return(a-b);
+};
+flatten2ndLevel();
